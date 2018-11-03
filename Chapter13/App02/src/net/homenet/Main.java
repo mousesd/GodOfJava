7f4d1f73e228c43143ea59e3ffd339ee1c2cb44e
Lee Sangdae <mousesd@gmail.com>
2018-11-03 16:32:55 +0900

    App02, Enum #2)

---
+++ Chapter13/App02/src/net/homenet/Main.java
@@ -0,0 +1,8 @@
+package net.homenet;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(OverTimeValue.FIVE_HOUR);
+        System.out.println(OverTimeValue.FIVE_HOUR.getAmount());
+    }
+}
