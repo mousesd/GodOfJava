fde1c6c8a689bd64dba71ed050530cb16a23918d
Lee Sangdae <mousesd@gmail.com>
2018-11-04 12:47:44 +0900

    App02, Enum #3)

--- Chapter13/App02/src/net/homenet/Main.java
+++ Chapter13/App02/src/net/homenet/Main.java
@@ -2,7 +2,14 @@ package net.homenet;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(OverTimeValue.FIVE_HOUR);
-        System.out.println(OverTimeValue.FIVE_HOUR.getAmount());
+        //# 1.
+        //System.out.println(OverTimeValue.FIVE_HOUR);
+        //System.out.println(OverTimeValue.FIVE_HOUR.getAmount());
+
+        //# 2.
+        OverTimeValue[] values = OverTimeValue.values();
+        for (OverTimeValue value : values) {
+            System.out.println(value);
+        }
     }
 }
