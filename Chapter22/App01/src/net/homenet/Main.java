b54cd7cd6cdd2d75930d7f3318235f7b401946e4
Lee Sangdae <mousesd@gmail.com>
2018-11-11 23:12:34 +0900

    App01, ArrayList #1)

---
+++ Chapter22/App01/src/net/homenet/Main.java
@@ -0,0 +1,12 @@
+package net.homenet;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("ArrayList Sample");
+
+        System.out.println(list.get(0));
+    }
+}
