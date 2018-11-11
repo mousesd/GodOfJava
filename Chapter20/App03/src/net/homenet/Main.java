15bed49cd95fe30a147f8a587367b6be30ba2392
Lee Sangdae <mousesd@gmail.com>
2018-11-11 16:12:39 +0900

    App03: Ex #1)

--- Chapter20/App03/src/net/homenet/Main.java
+++ Chapter20/App03/src/net/homenet/Main.java
@@ -2,9 +2,14 @@ package net.homenet;
 
 public class Main {
     public static void main(String[] args) {
+        //# 1.
         Main app = new Main();
         System.out.println(app.parseLong("r1024"));
         System.out.println(app.parseLong("1024"));
+
+        //# 2.
+        System.out.println();
+        app.printOtherBase(1024);
     }
 
     private long parseLong(String data) {
@@ -14,5 +19,12 @@ public class Main {
             System.out.println(data + "is not a number.");
             return -1;
         }
+    }
+
+    private void printOtherBase(long value) {
+        System.out.println("Origin: " + value);
+        System.out.println("Binary: " + Long.toBinaryString(value));
+        System.out.println("Hex: " + Long.toHexString(value));
+        System.out.println("Octal: " + Long.toOctalString(value));
     }
 }
