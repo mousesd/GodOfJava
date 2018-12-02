e051327aec67839db615901093f8f88b7812e64e
Lee Sangdae <mousesd@gmail.com>
2018-12-02 09:56:08 +0900

    App02, Thread.sleep()

---
+++ Chapter25/App02/src/net/homenet/EndlessThread.java
@@ -0,0 +1,15 @@
+package net.homenet;
+
+public class EndlessThread extends Thread {
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                System.out.println(System.currentTimeMillis());
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ Chapter25/App02/src/net/homenet/Main.java
@@ -0,0 +1,8 @@
+package net.homenet;
+
+public class Main {
+    public static void main(String[] args) {
+        EndlessThread thread = new EndlessThread();
+        thread.start();
+    }
+}
