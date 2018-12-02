da59754be6cad1afd7a1e720cde3229b9f6bce28
Lee Sangdae <mousesd@gmail.com>
2018-12-02 11:26:04 +0900

    App05, synchronize statement

--- Chapter25/App05/src/net/homenet/Calculate.java
+++ Chapter25/App05/src/net/homenet/Calculate.java
@@ -2,14 +2,29 @@ package net.homenet;
 
 @SuppressWarnings("WeakerAccess")
 public class Calculate {
+    private final Object lockObject = new Object();
     private int amount;
 
-    public synchronized void plus(int value) {
-        amount += value;
+    //# 1.synchronize method
+    //public synchronized void plus(int value) {
+    //    amount += value;
+    //}
+    //
+    //public synchronized void minus(int value) {
+    //    amount -= value;
+    //}
+
+    //# 2.synchronize statement
+    public void plus(int value) {
+        synchronized (lockObject) {
+            amount += value;
+        }
     }
 
-    public synchronized void minus(int value) {
-        amount -= value;
+    public void minus(int value) {
+        synchronized (lockObject) {
+            amount -= value;
+        }
     }
 
     public int getAmount() {
